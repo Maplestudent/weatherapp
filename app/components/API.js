231e62a2a97c8e9ba8d7f6006db265c2d51592dc
monkeyV10 <119895558+monkeyV10@users.noreply.github.com>
2023-12-08 19:41:29 -0700

    Update API.js

--- app/components/API.js
+++ app/components/API.js
@@ -1,13 +1,13 @@
 // API.js
 
 const BASE_URL = 'http://api.weatherapi.com/v1';
-const API_KEY = 'd403b6b07ce94c56ab5213347230412'; // Replace with your WeatherAPI.com API key
+const REACT_APP_WEATHER_API_KEY = 'd403b6b07ce94c56ab5213347230412'; // Replace with your WeatherAPI.com API key
 
 // Function to fetch 7-day weather forecast data
 export const get7DayWeatherForecast = async (location) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/forecast.json?key=${API_KEY}&q=${location}&days=7` // Request data for 7 days
+      `${BASE_URL}/forecast.json?key=${REACT_APP_WEATHER_API_KEY}&q=${location}&days=7` // Request data for 7 days
     );
 
     if (!response.ok) {
